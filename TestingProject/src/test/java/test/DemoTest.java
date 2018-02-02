57b49b10cca966a1ae437988d80e75dc4080ae7d
subat <subat@192.168.0.7>
2018-02-02 12:06:07 -0800

    adding new code

--- TestingProject/src/test/java/test/DemoTest.java
+++ TestingProject/src/test/java/test/DemoTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import library.common;
@@ -9,15 +8,13 @@ import library.common;
 public class DemoTest extends common{
 	
 	@Test
-	public void testWalmart() throws InterruptedException {
-		Actions move = new Actions(driver);
-		move.moveToElement(driver.findElement(By.id("header-GlobalAccountFlyout-flyout-link")));
+	public void testCostco() throws InterruptedException {
+		driver.findElement(By.id("header_sign_in")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.cssSelector("#header-GlobalAccountFlyout-flyout-link > div.header-GlobalAccountFlyout-customerName.dropdown-link"));
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/section/form/div[1]/div/label/div[2]/div/input")).sendKeys("21312@gmail.com");
-		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/section/form/div[2]/div/div[1]/label/div[2]/div/input")).sendKeys("123");
-		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/section/form/div[5]/button")).click();
+		driver.findElement(By.id("logonId")).sendKeys("123123@gmail.com");
+		driver.findElement(By.id("logonPassword")).sendKeys("123123");
+		driver.findElement(By.className("primary")).click();;
+		
 	}
 
 }
